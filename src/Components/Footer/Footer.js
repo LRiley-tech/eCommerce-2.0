7cea4f253d18a7fdc53383e39fa526efecb3fac8
Leeangelo Riley <59509041+LeCoding-tech@users.noreply.github.com>
2021-05-29 00:25:43 -0400

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,9 +5,6 @@ class Footer extends React.Component {
     return (
       <div>
         <head>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
-                integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous" />
-                <link rel="stylesheet" href="https://use.fontwesome.com/releases/v5.4.1/css/all.css" />
         </head>
           <footer id="footer">
           <div className="footer--text">
